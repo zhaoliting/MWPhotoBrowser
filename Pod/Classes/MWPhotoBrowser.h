8a8537bcdc3ebff8c6596f29fee8a1e5f0636ae3
zhaoliting <810692254@qq.com>
2022-08-02 17:10:21 +0800

    去掉UIActionSheetDelegate

--- Pod/Classes/MWPhotoBrowser.h
+++ Pod/Classes/MWPhotoBrowser.h
@@ -38,7 +38,7 @@
 
 @end
 
-@interface MWPhotoBrowser : UIViewController <UIScrollViewDelegate, UIActionSheetDelegate>
+@interface MWPhotoBrowser : UIViewController <UIScrollViewDelegate>
 
 @property (nonatomic, weak) IBOutlet id<MWPhotoBrowserDelegate> delegate;
 @property (nonatomic) BOOL zoomPhotosToFill;
